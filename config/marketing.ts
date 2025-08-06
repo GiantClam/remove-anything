766ccb9cae5a778e8d57e6b32fc5841a8b9eddeb
bei <liulanggoukk@gmail.com>
2025-08-06 22:11:04 +0800

    [bug]fix bug

--- config/marketing.ts
+++ config/marketing.ts
@@ -3,6 +3,14 @@ import { MarketingConfig } from "types";
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
+      title: "BackgroundRemoval",
+      href: "/app/generate",
+    },
+    {
+      title: "BatchBackgroundRemoval",
+      href: "/app/batch-remove-background",
+    },
+    {
       title: "explore",
       href: "/explore",
     },
